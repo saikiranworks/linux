d4f3efb9c058d10a5a154327f754146e6ef8a78e
Jens Glathe <jens.glathe@oldschoolsolutions.biz>
2025-06-27 08:43:53 +0200

    drm/panel-edp: add panel SDC 0x4187 ATNA40CT01
    mainly to avoid the splat for now.
    
    Signed-off-by: Jens Glathe <jens.glathe@oldschoolsolutions.biz>

--- drivers/gpu/drm/panel/panel-edp.c
+++ drivers/gpu/drm/panel/panel-edp.c
@@ -2197,6 +2197,8 @@ static const struct edp_panel_entry edp_panels[] = {
 	EDP_PANEL_ENTRY('S', 'H', 'P', 0x1593, &delay_200_500_p2e100, "LQ134N1"),
 	EDP_PANEL_ENTRY('S', 'H', 'P', 0x15a7, &delay_200_500_e200, "LQ120P1JX51"),
 
+	EDP_PANEL_ENTRY('S', 'D', 'C', 0x4187, &delay_200_500_e80_d50, "ATNA40CT01"),
+
 	EDP_PANEL_ENTRY('S', 'T', 'A', 0x0004, &delay_200_500_e200, "116KHD024006"),
 	EDP_PANEL_ENTRY('S', 'T', 'A', 0x0009, &delay_200_500_e250, "116QHD024002"),
 	EDP_PANEL_ENTRY('S', 'T', 'A', 0x0100, &delay_100_500_e200, "2081116HHD028001-51D"),
