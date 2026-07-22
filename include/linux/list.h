dd95c4d5f0ee66650df6c0b9baa06d26f6034a13
Eric Naim <dnaim@cachyos.org>
2026-07-22 15:46:01 +0700

    list: Mark some functions as __always_inline
    Eric Dumazet found that when the kernel is compiled with clang,
    list_add() and friends are not inlined into calling functions. Mark
    these functions as __always_inline to improve performance and have a
    smaller binary as a result.
    
    Link: https://lore.kernel.org/all/CANn89iJVQe=wedLheJmjZjOTJsWHijT0jZs=iRxKssJZbjAxHw@mail.gmail.com/
    Signed-off-by: Eric Naim <dnaim@cachyos.org>

--- include/linux/list.h
+++ include/linux/list.h
@@ -201,7 +201,7 @@ static __always_inline void list_add(struct list_head *new,
  * Insert a new entry before the specified head.
  * This is useful for implementing queues.
  */
-static inline void list_add_tail(struct list_head *new, struct list_head *head)
+static __always_inline void list_add_tail(struct list_head *new, struct list_head *head)
 {
 	__list_add(new, head->prev, head);
 }
@@ -236,7 +236,7 @@ static inline void list_add_tail_release(struct list_head *new,
  * This is only for internal list manipulation where we know
  * the prev/next entries already!
  */
-static inline void __list_del(struct list_head * prev, struct list_head * next)
+static __always_inline void __list_del(struct list_head * prev, struct list_head * next)
 {
 	next->prev = prev;
 	WRITE_ONCE(prev->next, next);
@@ -250,13 +250,13 @@ static inline void __list_del(struct list_head * prev, struct list_head * next)
  * WRITE_ONCE() overhead of a regular list_del_init(). The code that uses this
  * needs to check the node 'prev' pointer instead of calling list_empty().
  */
-static inline void __list_del_clearprev(struct list_head *entry)
+static __always_inline void __list_del_clearprev(struct list_head *entry)
 {
 	__list_del(entry->prev, entry->next);
 	entry->prev = NULL;
 }
 
-static inline void __list_del_entry(struct list_head *entry)
+static __always_inline void __list_del_entry(struct list_head *entry)
 {
 	if (!__list_del_entry_valid(entry))
 		return;
@@ -270,7 +270,7 @@ static inline void __list_del_entry(struct list_head *entry)
  * Note: list_empty() on entry does not return true after this, the entry is
  * in an undefined state.
  */
-static inline void list_del(struct list_head *entry)
+static __always_inline void list_del(struct list_head *entry)
 {
 	__list_del_entry(entry);
 	entry->next = LIST_POISON1;
